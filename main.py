7e1677da2e6bdd69a15edb2cec5dc72a001c2500
OlgaRv <olgavr2959@gmail.com>
2024-07-24 21:16:42 +0300

    New code

--- main.py
+++ main.py
@@ -1,2 +1,34 @@
+def add(a, b):
+    return a + b
+
+
+def subtract(a, b):
+    return a - b
+
+
+def multiply(a, b):
+    return a * b
+
+
+def subtract(a, b):
+    return a - b
+
+
+def mulitiply(a, b):
+    return a * b
+
+
+def divide(a, b):
+    if b == 0:
+        raise ValueError('На ноль делить нельзя!')
+    return a / b
+
+
 def check(number):
     return number % 2 == 0
+
+
+def remainder(a, b):   #  Функция для вычисления остатка от деления a на b.
+    if b == 0:
+        raise ValueError("Деление на ноль недопустимо.")
+    return a % b
--- test.py
+++ test.py
@@ -1,5 +1,6 @@
+
 import unittest
-from main import check
+from main import check, add, subtract, multiply, divide, remainder
 
 class TestCheck(unittest.TestCase):
     def test_check(self):
@@ -15,6 +16,39 @@ class TestCheck(unittest.TestCase):
         self.assertFalse(check(1))
         self.assertFalse(check(5))
         self.assertFalse(check(9))
+
+    def test_add(self):
+        self.assertEqual(add(10, 5), 15)
+        self.assertNotEqual(add(10, -5), 4)
+        self.assertEqual(add(-10, -5), -15)
+
+    def test_subtract(self):
+        self.assertEqual(subtract(10, 5), 5)
+        self.assertNotEqual(subtract(10, -5), 13)
+        self.assertEqual(subtract(-10, -5), -5)
+
+    def test_multiply(self):
+        self.assertEqual(multiply(10, 5), 50)
+        self.assertNotEqual(multiply(10, -5), -52)
+        self.assertEqual(multiply(-10, -5), 50)
+
+    def test_divide_success(self):
+        self.assertEqual(divide(10, 5), 2)
+        self.assertNotEqual(divide(10, -5), -3)
+        self.assertEqual(divide(-10, -5), 2)
+
+    def test_remainder(self):
+        self.assertEqual(remainder(10, 5), 0)
+        self.assertEqual(remainder(10, -5), 0)
+        self.assertEqual(remainder(10, 1), 0)
+
+    def test_remainder_by_zero(self):
+        self.assertRaises(ValueError, remainder, 10, 0)
+
+    def test_divide_by_zero(self):
+        with self.assertRaises(ValueError):
+            divide(10, 0)  # правильная исключительная ситуация
+
 
 if __name__ == '__main__':
     unittest.main()
